4770d165ad2a86d1bd390416d9ecc0485a3c31ac
Charles Benfer <czb0146@auburn.edu>
2024-03-15 13:45:03 -0500

    Shap value code updated, will run on home PC

--- Chicken Bacteria/Shapley Values.R
+++ Chicken Bacteria/Shapley Values.R
@@ -2,6 +2,7 @@
 
 library(fastshap)
 library(tibble)
+library(shapviz)
 
 pfun <- function(object, newdata) {  # prediction wrapper
   predict(object, newdata, type='prob', predict.all=F)
@@ -14,8 +15,8 @@ pred <- pfun(rf_TET_2,rand_pred)[1,1]
 
 X <- subset(Chick_Sal_ARA_mgKg, select = -MIC_TET)
 
-shaps <- explain(rf_TET_2, X = X, pred_wrapper = pfun, nsim = 100, adjust = TRUE,
-                           shap_only = FALSE)
+shaps <- explain(rf_TET_2, X = X, pred_wrapper = pfun, nsim = 100,
+                 adjust = TRUE, shap_only = FALSE)
 
 tibble::as_tibble(shaps$shapley_values)
 
