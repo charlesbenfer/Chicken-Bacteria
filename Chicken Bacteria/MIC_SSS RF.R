97f2da5ad7a2269655fe482e93cd87648dafc8d2
Charles Benfer <czb0146@auburn.edu>
2024-03-15 09:39:46 -0500

    Added some variable name changes but nothing major

--- Chicken Bacteria/MIC_SSS RF.R
+++ Chicken Bacteria/MIC_SSS RF.R
@@ -12,8 +12,8 @@ rf_SSS_2 <- randomForest(as.formula(paste(colnames(Chick_Sal_ARA_mgKg)[22],
                                                   collapse = "+"),sep = "")),
                        data = train, ntree=1000,keep.forest=T,importance=T)
 
-importance(rf_SSS,type=1)
-varImpPlot(rf_SSS)
+importance(rf_SSS_2,type=1)
+varImpPlot(rf_SSS_2)
 rf_SSS_2$confusion
 # Final ID Serotype has massive comparative accuracy decrease, what is this variable?
 
@@ -51,14 +51,14 @@ levels(Chick_Sal_ARA_mgKg_SSS[,22])
 #Run another RF with the more diverse labels
 
 ind <- sample(2, nrow(Chick_Sal_ARA_mgKg_SSS), replace = T,prob = c(.7,.3))
-train <- Chick_Sal_ARA_mgKg_SSS[ind==1,]
-test <- Chick_Sal_ARA_mgKg_SSS[ind==2,]
+train_SSS <- Chick_Sal_ARA_mgKg_SSS[ind==1,]
+test_SSS <- Chick_Sal_ARA_mgKg_SSS[ind==2,]
 
 #Train a RF for MIC_SSS on the train data based on columns 40-56
 rf_SSS_5 <- randomForest(as.formula(paste(colnames(Chick_Sal_ARA_mgKg_SSS)[22],
                                         "~",paste(colnames(Chick_Sal_ARA_mgKg_SSS)[c(1:21,23:39,41:56)],
                                                   collapse = "+"),sep = "")),
-                       data = train, ntree=1000,keep.forest=T,importance=T)
+                       data = train_SSS, ntree=1000,keep.forest=T,importance=T)
 
 importance(rf_SSS_5,type=1)
 varImpPlot(rf_SSS_5)
@@ -67,6 +67,6 @@ rf_SSS_5$confusion
 #guidance on how to split more effectively may be necessary
 
 #Prediction with generated random forest
-SSS_preds_5 <- predict(rf_SSS_5, newdata = test)
-confusionMatrix(test[,22],SSS_preds_5)
+SSS_preds_5 <- predict(rf_SSS_5, newdata = test_SSS)
+confusionMatrix(test_SSS[,22],SSS_preds_5)
 #Similar to above in 66
