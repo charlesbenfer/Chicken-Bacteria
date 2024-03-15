490e42ebd95a8eeb52c48a2874a884449a508d29
Charles Benfer <czb0146@auburn.edu>
2024-03-15 11:00:03 -0500

    Added shapley values R file

---
+++ Chicken Bacteria/Shapley Values.R
@@ -0,0 +1,22 @@
+#Shapley Values
+
+library(fastshap)
+library(tibble)
+
+pfun <- function(object, newdata) {  # prediction wrapper
+  predict(object, newdata, type='prob', predict.all=F)
+}
+
+rand_row <- sample(1:nrow(Chick_Sal_ARA_mgKg),size=1)
+rand_pred <- Chick_Sal_ARA_mgKg[rand_row,]
+
+pred <- pfun(rf_TET_2,rand_pred)[1,1]
+
+X <- subset(Chick_Sal_ARA_mgKg, select = -MIC_TET)
+
+shaps <- explain(rf_TET_2, X = X, pred_wrapper = pfun, nsim = 100, adjust = TRUE,
+                           shap_only = FALSE)
+
+tibble::as_tibble(shaps$shapley_values)
+
+
